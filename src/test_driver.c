e6289a3ac5f23d4afe1ce772ba8ce8419c484dcd
Brian Hoenes <bhoenes@mines.edu>
2012-03-28 16:21:49 -0600

    passes comparison tests. part 2 complete.  need to modify makefile and create shell driver script to test different levels of optimization.

--- src/test_driver.c
+++ src/test_driver.c
@@ -58,64 +58,62 @@ void print_square_matrix(const int n, const double a[]) {
   }
 }
 
+int calc_mflops(const int flops_per_iteration, const int n, const double elapsed_time) {
+  return (int)((0.000001*n*n*n*flops_per_iteration)/elapsed_time);
+}
+
 // *******************Test Driver********************************
 
 int main(void) {
-	const int size[NSTEPS] = {2, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};  // specification of array sizes
-	static const double maxtime = 2.0; // number of seconds to time each step size
+	const int size[NSTEPS] = {500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};  // specification of array sizes
 	static const double epsilon = 0.00001; // maximum floating point error allowed between doubles to be considered equal
+	const double flops_per_iteration = 2.0;
 	
-	int mult_count;  // count of number of multiplications  
 	int i;  // index variables for looping
-	int mflops[2];  // megaflops calculation 0 is test case, 1 is base case
+	double time[2]; // elapsed time. 0 is test case, 1 is base case
+	int mflops[2]; // calculated mflops.  0 is test case, 1 is base case
 	double* a;
 	double* b;
 	double* ctest;
 	double* cbase;
 	stopwatch* sw = stopwatch_new();
 	
-	for(i = 0; i < 1; i++) {
+	for(i = 0; i < NSTEPS; i++) {
 	  int n = size[i];
 	  int n2 = n * n;
 	  a = (double*) malloc(n2*sizeof(double));
  	  b = (double*) malloc(n2*sizeof(double));
  	  ctest = (double*) malloc(n2*sizeof(double));
  	  cbase = (double*) malloc(n2*sizeof(double));
- 	  rand_square_double_matrix(i, n, a);
- 	  rand_square_double_matrix(i+1, n, b);
+ 	  rand_square_double_matrix(i+10, n, a);
+ 	  rand_square_double_matrix(i+11, n, b);
     zero_square_double_matrix(n, ctest);
 	  zero_square_double_matrix(n, cbase);
 	  	  
-	  mult_count = 0;
 	  stopwatch_restart(sw);
-	  while(stopwatch_time(sw) < maxtime) {
-      my_dgemm(n, a, b, ctest);
-      mult_count++;
-		}
+    my_dgemm(n, a, b, ctest);
 	  stopwatch_stop(sw);
-	  mflops[0] = (int)((0.000002*n2*mult_count)/stopwatch_time(sw));
+    time[0] = stopwatch_time(sw);
 	  
-	  mult_count = 0;
 	  stopwatch_restart(sw);
-	  while(stopwatch_time(sw) < maxtime) {
-      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, a, n, b, n, 1.0, cbase, n);
-      mult_count++;
-		}
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, a, n, b, n, 1.0, cbase, n);
 	  stopwatch_stop(sw);
 	  
-	  printf("A\n");
-	  print_square_matrix(n, a);
-	  printf("B\n");
-	  print_square_matrix(n, b);	  
-	  printf("my_dgemm\n");
-	  print_square_matrix(n, ctest);
-	  printf("cdgemm\n");
-	  print_square_matrix(n, cbase);
+	  //printf("A\n");
+	  //print_square_matrix(n, a);
+	  //printf("B\n");
+	  //print_square_matrix(n, b);	  
+	  //printf("my_dgemm\n");
+	  //print_square_matrix(n, ctest);
+	  //printf("cdgemm\n");
+	  //print_square_matrix(n, cbase);
 	  
 	  assert_equal(n, ctest, cbase, epsilon);
-	  mflops[1] = (int)((0.000002*n2*mult_count)/stopwatch_time(sw));
-	  
-	  printf("%d, %d, %d\n", n, mflops[0], mflops[1]);
+    time[1] = stopwatch_time(sw);
+    mflops[0] = calc_mflops(flops_per_iteration, n, time[0]);
+    mflops[1] = calc_mflops(flops_per_iteration, n, time[1]);
+    	  
+	  printf("%d, %5.2f, %d, %5.2f, %d\n", n, time[0], mflops[0], time[1], mflops[1]);
 
     free(a);
     free(b);
